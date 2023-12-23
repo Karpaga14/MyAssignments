d624c258417718d846d291371500079e34deb909
Karpaga14 <154664717+Karpaga14@users.noreply.github.com>
2023-12-23 14:42:35 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("Browser Version is : "+browserVersion);
+		System.out.println("Browser Name is : "+browserName);
+		System.out.println("Is Visible : "+isVisible);
+		System.out.println("Released Year is : "+releaseYear);
+		System.out.println("Browser Logo is : "+browserLogo);
+	}
+
+}
