32321dc50dd4985cf7b2a871c9dd6a1794010721
Karpaga14 <154664717+Karpaga14@users.noreply.github.com>
2023-12-24 14:15:47 +0530

    Add files via upload
    Day 1 Assignment

---
+++ day1/BrowserClass.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class BrowserClass {
+	/*Assignment 1: 
+
+		Goal: To understand the class, main method and sysout
+
+		a) Create a class by name: Browser
+		b) Create a main method using shortcut
+		c) print a statement --> "This is my Browser"
+		d) run and confirm the printed in console*/
+public static void main(String[] args) {
+	System.out.println("This is my browser");
+}
+}
