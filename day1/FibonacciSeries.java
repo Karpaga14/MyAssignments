6d10489abce1826ae35f98a38375f5e4bb5d71e7
Karpaga14 <154664717+Karpaga14@users.noreply.github.com>
2023-12-26 22:04:54 +0530

    Add files via upload
    Week2Day1 Assignments

---
+++ day1/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+	int a=0;
+	int b=1;
+	int n=8;
+	System.out.println("Fibonacci Series");
+	for(int i=1;i<=n;i++)
+	{
+		int c=a+b;
+		a=b;
+		b=c;
+		System.out.println(c);
+	}
+
+	}
+
+}
---
+++ day1/FindOddNumbers.java
@@ -0,0 +1,28 @@
+package week2.day1;
+
+public class FindOddNumbers {
+	/*Create a java class named FindOddNumbers
+- Declare a with integer variable as maxRange with value 10
+- Use for loop to iterate
+- Divide the value and check the reminder is equal to 1 using if condition*/
+
+	public static void main(String[] args) {
+		FindOddNumbers oddNo = new FindOddNumbers();
+		oddNo.printOddNumbers(1,10);
+		
+	}
+	
+	
+	public void printOddNumbers(int intialization,int maxRange)
+	{
+		System.out.println("Odd Numbers from "+intialization + "to "+maxRange);
+		for (int i=intialization;i<=maxRange;i++)
+		{
+			if((i%2)==1)
+			{
+				System.out.println(i);
+		}
+	}
+
+}
+}
